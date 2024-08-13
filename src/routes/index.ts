5ea3a3174ad734d7f096eae0d228b0a03468db1b
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2024-08-13 14:45:05 +0200

    project update

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,9 @@ import { Route } from './Route';
 import { Controller } from '../controllers/Controller';
 import { ProtectedRoute } from './ProtectedRoute';
 
-
 /* @formatter:off */
 export const createRoutes = (authController: Controller): Route[] => [
-
   // User routes
   new Route('post', '/login', (req, res) => authController.login(req, res)),
-  new Route('post', '/register', (req, res) => authController.register(req, res))];
+  new Route('post', '/register', (req, res) => authController.register(req, res)),
+];
