f002cddf6425f8f10013139c812e0ac36046199f
Maciej Ostrowski <maciej.ostrowski.it@icloud.com>
2024-08-13 14:42:56 +0200

    project update

--- src/mappers/MongoAuthSerializer.ts
+++ src/mappers/MongoAuthSerializer.ts
@@ -4,12 +4,18 @@ import { Serializer } from './Serializer';
 
 //TransactionDb powinno nazywac sie TransactionDbSchema
 export class MongoAuthSerializer implements Serializer<UserDb, User> {
-
   mapToEntity(document: UserDb): User {
-    return new User(document._id, document.email, document.username, document.password, document.accountId, document.status);
+    return new User(
+      document._id,
+      document.email,
+      document.username,
+      document.password,
+      document.accountId,
+      document.status
+    );
   }
 
   mapToDb(entity: User): UserDb {
-    return {...entity.mapToDb() };
+    return { ...entity.mapToDb() };
   }
 }
